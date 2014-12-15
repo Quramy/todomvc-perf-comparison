6cfd00bfec759e5fd7acbc401bea88ca74e32b43
Yosuke Kurami <yosuke.kurami@gmail.com>
2014-12-16 03:20:31 +0900

    Add Flux test

--- resources/tests.js
+++ resources/tests.js
@@ -153,6 +153,41 @@ Suites.push({
 });
 
 Suites.push({
+    name: 'Flux React',
+    url: 'todomvc/flux-react/index.html',
+    version: '0.12.0',
+    prepare: function (runner, contentWindow, contentDocument) {
+        contentWindow.Utils.store = function () {}
+        return runner.waitForElement('#new-todo').then(function (element) {
+            element.focus();
+            return element;
+        });
+    },
+    tests: [
+        new BenchmarkTestStep('Adding' + numberOfItemsToAdd + 'Items', function (newTodo, contentWindow, contentDocument) {
+            for (var i = 0; i < numberOfItemsToAdd; i++) {
+                var keydownEvent = document.createEvent('Event');
+                keydownEvent.initEvent('keydown', true, true);
+                keydownEvent.which = 13; // VK_ENTER
+                newTodo.value = 'Something to do ' + i;
+                newTodo.dispatchEvent(keydownEvent);
+            }
+        }),
+        new BenchmarkTestStep('CompletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var checkboxes = contentDocument.querySelectorAll('.toggle');
+            for (var i = 0; i < checkboxes.length; i++)
+                checkboxes[i].click();
+        }),
+        new BenchmarkTestStep('DeletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var deleteButtons = contentDocument.querySelectorAll('.destroy');
+            for (var i = 0; i < deleteButtons.length; i++)
+                deleteButtons[i].click();
+        })
+    ]
+});
+
+
+Suites.push({
     name: 'Om',
     url: 'todomvc/om/index.html',
     version: '0.5.0 + React 0.9.0',
